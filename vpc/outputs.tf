37769330e7a503d4df9120213b6f5857614b6eff
Te L <lite2073+git@gmail.com>
2026-01-01 19:39:32 -0800

    output private subnets

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -7,3 +7,8 @@ output "public_subnets" {
   description = "public subnet IDs"
   value       = aws_subnet.public_subnets[*].id
 }
+
+output "private_subnets" {
+  description = "private subnet IDs"
+  value       = aws_subnet.private_subnets[*].id
+}
