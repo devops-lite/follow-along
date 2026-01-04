04731bfbd2c7f5b68291f61161f45d9e56e3abe4
Te L <lite2073+git@gmail.com>
2026-01-04 15:39:01 -0800

    simplify

--- modules/data/iam_user.tf
+++ modules/data/iam_user.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  iam_user = element(regex("arn:aws:iam::\\d+:user/(.*)$", data.aws_caller_identity.current.arn), 0)
+  iam_user = regex("arn:aws:iam::\\d+:user/(.*)$", data.aws_caller_identity.current.arn)[0]
   env      = var.env != "" ? var.env : local.iam_user
 }
